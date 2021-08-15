ed53d7fe09dc7c12d075fc40af1dfb756f2917f5
realrecogreal <spdlqjqlwjs@naver.com>
2021-08-15 17:07:58 +0900

    hellyeah 메시지 추

--- src/FxxkTheWorld.java
+++ src/FxxkTheWorld.java
@@ -3,6 +3,7 @@ public class FxxkTheWorld {
 	public static void main(String[] args) {
 		System.out.println("Fxxk the world!");
 		System.out.println("damn");
+		System.out.println("hellyeah");
 	}
 	
 }
